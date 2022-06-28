d8146a605d4aeafcf489960ee8757c03a6db603a
Kaique <kaiquesouzafarias@outlook.com>
2022-06-28 17:57:29 -0300

    Adicionei o Typescript no projeto

---
+++ dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.use(express_1.default.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path_1.default.join(__dirname, '../views'));
+app.get('/', (_req, res) => {
+    res.render('index');
+});
+app.get("/news/");
+app.listen(port, () => {
+    console.log('listening on port ' + port);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
-const express = require('express');
-const path = require('path');
+import path from "path";
+import express, {
+    Request,
+    Response,
+} from "express"
 
 const app = express();
 const port = 3000;
@@ -9,7 +12,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
-app.get('/', (req, res) => {
+app.get('/', (_req: Request, res: Response) => {
     res.render('index');
 });
 
